2afff96f5aec6760cd2da2871be3496b78b5e37b
Leo Dion <leogdion@brightdigit.com>
2016-10-01 20:14:47 -0400

    removed bad comment

--- Formula/speculid.rb
+++ Formula/speculid.rb
@@ -9,12 +9,10 @@ class Speculid < Formula
   version "1.0.0-alpha6"
   sha256 "7402c76f832fa9427fe5474b87d3858452b49f3702c3cb529c56511522d1f252"
 
-  # depends_on "cmake" => :build
   depends_on "homebrew/gui/inkscape"
   depends_on "imagemagick"
   depends_on :xcode => :build
   depends_on :macos => :yosemite
-
 
   def install
     xcodebuild "-workspace", "speculid.xcworkspace", "-scheme", "Speculid", "-derivedDataPath", buildpath, "build", "-configuration", "Release", "CODE_SIGNING_REQUIRED=NO"
@@ -23,7 +21,6 @@ class Speculid < Formula
     bin.install_symlink prefix/"Speculid.app/Contents/MacOS/Speculid" => "speculid"
   end
   
-
   test do
     # `test do` will create, run in and delete a temporary directory.
     #
