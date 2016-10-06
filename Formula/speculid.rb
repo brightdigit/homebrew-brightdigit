d2d2b00a72a7cdd98987627b021851b3a4483591
Leo Dion <leogdion@brightdigit.com>
2016-10-06 10:50:49 -0400

    fixed formula

--- Formula/speculid.rb
+++ Formula/speculid.rb
@@ -1,9 +1,9 @@
 class Speculid < Formula
   desc "Create Raster Images for Xcode Assets"
   homepage "http://www.speculid.com"
-  url "https://github.com/brightdigit/speculid/archive/1.0.0-alpha7.tar.gz"
-  version "1.0.0-alpha7"
-  sha256 "d8aadd5d633a3c8b62b687edfabe469173fe302d4b90c9c959e958555e08162e"
+  url "https://github.com/brightdigit/speculid/archive/1.0.0-alpha12.tar.gz"
+  version "1.0.0-alpha12"
+  sha256 "ee09a145abb627f05b982db3e00eeb75792874f417ce401a45a1249bb2473596"
 
   depends_on "homebrew/gui/inkscape"
   depends_on "imagemagick"
@@ -11,12 +11,17 @@ class Speculid < Formula
   depends_on :macos => :yosemite
 
   def install
-    xcodebuild "-workspace", "speculid.xcworkspace", "-scheme", "Speculid", "-derivedDataPath", buildpath, "build", "-configuration", "Release", "SYMROOT=build", "CODE_SIGNING_REQUIRED=NO"
+    xcodebuild "-workspace", "speculid.xcworkspace", "-scheme", "Speculid Application", "-derivedDataPath", buildpath, "build", "-configuration", "Release", "SYMROOT=#{buildpath}/Build/Products", "CODE_SIGNING_REQUIRED=NO"
+    system "zip", "-r", "examples.zip", "examples"
+    prefix.install "examples.zip"
+    prefix.install "shasum.sh"
     prefix.install "#{buildpath}/Build/Products/Release/Speculid.app"
     bin.install_symlink prefix/"Speculid.app/Contents/MacOS/Speculid" => "speculid"
   end
 
   test do
-    system "#{bin}/speculid", "-h"
+    system "unzip", "#{prefix}/examples.zip", "-d", testpath
+    assert_equal shell_output("cat #{testpath}/examples/shasum"), shell_output("#{prefix}/shasum.sh #{bin}/speculid #{testpath}/examples/Assets")
+    system "#{bin}/speculid", "--version"
   end
 end
