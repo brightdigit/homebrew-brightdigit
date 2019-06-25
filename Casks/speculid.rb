77e458d939cbc7d2f8d94d3a494b408dd5cc9d6c
leogdion <leogdion@brightdigit.com>
2019-06-25 13:57:30 -0400

    Update speculid.rb

--- Casks/speculid.rb
+++ Casks/speculid.rb
@@ -1,9 +1,9 @@
 cask 'speculid' do
-  version '2.0.0-alpha.3'
-  sha256 'a42856845a422893e0493638c8ae8f6ea282b74a80075474f527836a4d5ae605'
+  version '2.0.0-alpha.4'
+  sha256 'b81bda9932c290549b510ed01ae5eba59b4eca501e2060b46b3fe35fc221f1ed'
 
   # github.com/brightdigit/Speculid was verified as official when first introduced to the cask
-  url 'https://github.com/brightdigit/Speculid/releases/download/v2.0.0-alpha.3/Speculid.zip'
+  url 'https://github.com/brightdigit/Speculid/releases/download/v2.0.0-alpha.4/Speculid.zip'
   appcast 'https://github.com/brightdigit/Speculid/releases.atom'
   name 'Speculid'
   homepage 'https://speculid.com/'
