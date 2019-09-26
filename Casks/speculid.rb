3e5812c3dbff6c9dda1192717d3dbbb1c010ccae
Leo Dion <leogdion@brightdigit.com>
2019-09-26 11:10:52 -0400

    upgrading homebrew

--- Casks/speculid.rb
+++ Casks/speculid.rb
@@ -1,9 +1,9 @@
 cask 'speculid' do
-  version '2.0.0-alpha.4'
-  sha256 'b81bda9932c290549b510ed01ae5eba59b4eca501e2060b46b3fe35fc221f1ed'
+  version '2.0.0-alpha.5'
+  sha256 '9ce9da244caf28e4871de3c138ae61956d362de5d916ee52e24e5844766ddfe4'
 
   # github.com/brightdigit/Speculid was verified as official when first introduced to the cask
-  url 'https://github.com/brightdigit/Speculid/releases/download/v2.0.0-alpha.4/Speculid.zip'
+  url 'https://github.com/brightdigit/Speculid/releases/download/v2.0.0-alpha.5/Speculid_2.0_alpha_5.zip'
   appcast 'https://github.com/brightdigit/Speculid/releases.atom'
   name 'Speculid'
   homepage 'https://speculid.com/'
